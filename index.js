c8ce4c2785a7dcb41e1768710259791b64674e52
pradumna-sinha <64945190+pradumna-sinha@users.noreply.github.com>
2021-06-16 16:30:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber1 * 6;
+randomNumber1 = Math.floor(randomNumber1 + 1);
+
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber2 * 6;
+randomNumber2 = Math.floor(randomNumber2 + 1);
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML = "Draw Game";
+}
